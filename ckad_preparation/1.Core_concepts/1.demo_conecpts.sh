a0a2298413891811dec8775061a9e2d986deea7a
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-10-27 06:36:33 +0800

    :white_check_mark: CKAD preparation :white_check_mark:

--- ckad_preparation/1.Core_concepts/1.demo_conecpts.sh
+++ ckad_preparation/1.Core_concepts/1.demo_conecpts.sh
@@ -1,1 +1,9 @@
-# Demo
+when we install k8s we will be installing below k8s components
+
+1. An API-Server --> Acts as front-end for kubernetes.The Users, Management Devices, Commandline  interfaces all talk to the API Server to interact with kubernetes cluster.
+2. An ETCD service --> Distributed Reliable key-value store info about multiple clusters and resposible maintain locks as well.
+3. A Kubelet service.
+4. A Container Runtime.
+5. Controllers.
+6. Schedulers --> Distribute containers or apps across multiple nodes, it looks for newly created containers and assigns them to nodes.
+
